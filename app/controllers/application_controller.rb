bb01a77817dcf9269c9897075828936519ca76d3
kodama0621 <eryuhikari@gmail.com>
2021-05-10 11:44:20 +0000

    [Add]deviseモデル'user'追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sin_up, keys: [:name])
+  end
+
 end

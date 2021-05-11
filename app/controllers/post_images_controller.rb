4b04ba68fa12f5bbe2e2fabb6195f4303b0e426e
kodama0621 <eryuhikari@gmail.com>
2021-05-11 09:25:36 +0000

    [Add]削除機能追加

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -15,10 +15,13 @@ class PostImagesController < ApplicationController
   end
 
   def show
-    @post_image = PostImage.find(prams[:id])
+    @post_image = PostImage.find(params[:id])
   end
 
   def destroy
+    @post_image = PostImage.find(params[:id])
+    @post_image.destroy
+    redirect_to post_images_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'homes#top'
+  root 'homes#top'
   get 'about/' => 'homes#about'
   devise_for :users
   resources :post_images, only: [:new, :create, :index, :show, :destroy]

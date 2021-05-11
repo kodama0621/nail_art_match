3004c3ca8ae35cb1106c919a058fa03ba0f6a541
kodama0621 <eryuhikari@gmail.com>
2021-05-11 14:39:28 +0000

    [Add]Likeモデル追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'about/' => 'homes#about'
   devise_for :users
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :likes, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
 

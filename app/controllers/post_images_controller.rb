258dbe2a2f6d25579f890b167e646358fa54838b
kodama0621 <eryuhikari@gmail.com>
2021-05-11 06:13:59 +0000

    [Update]postimageコントローラ更新

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,17 +1,28 @@
 class PostImagesController < ApplicationController
   def new
+    @post_image = ImagePost.new
   end
 
   def create
+    @postimage = PostImage.new(post_image_params)
+    @post_image.user_id = current_user.id
+    @post_image.save
+    redirect_to post_images_path
   end
 
   def index
+    @post_images = PostImage.all
   end
 
   def show
   end
 
   def destroy
+  end
+
+  private
+  def post_image_params
+    params.require(:post_image).permit(:title, :image, :caption)
   end
 
 

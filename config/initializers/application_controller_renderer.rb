6f8df8e34bdb7d5bdffc771c1a1b7b82def5c2ad
kodama0621 <eryuhikari@gmail.com>
2021-05-11 07:18:59 +0000

    [Update]投稿一覧追加,コントローラ修正,layouts新規投稿リンク修正

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '2135c075688f8d5a05c8ab4f263edf343f226b0ee0a59ad3ddaea0d8eccfde54d533d81f2337d7196190a52c3a2353245c47d720b3face293058e638f2a7ae89'

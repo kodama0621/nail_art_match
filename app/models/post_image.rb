d04599602559332c82741ccc6753f96d9aa8f7d6
kodama0621 <eryuhikari@gmail.com>
2021-05-11 05:55:30 +0000

    [Update]新規投稿ページ作成

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,3 +1,4 @@
 class PostImage < ApplicationRecord
   belongs_to :user
+  attachment :image
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: 'homes#top'
   get 'about/' => 'homes#about'
+  devise_for :users
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
 end

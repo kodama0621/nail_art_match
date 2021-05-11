dbe0e91058a5262e53f40cbdeb9dce6e7de8ae3c
kodama0621 <eryuhikari@gmail.com>
2021-05-11 04:34:14 +0000

    [Add]postimageモデル追加

---
+++ db/migrate/20210511043315_create_post_images.rb
@@ -0,0 +1,12 @@
+class CreatePostImages < ActiveRecord::Migration[5.2]
+  def change
+    create_table :post_images do |t|
+      t.integer :user_id
+      t.string :image_id
+      t.string :title
+      t.text :caption
+
+      t.timestamps
+    end
+  end
+end

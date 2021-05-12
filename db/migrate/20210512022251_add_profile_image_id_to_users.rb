6da990d9739161eb3de5a4f37b39ff29764157b4
kodama0621 <eryuhikari@gmail.com>
2021-05-12 02:33:11 +0000

    [Add]プロフィールカラム追加

---
+++ db/migrate/20210512022251_add_profile_image_id_to_users.rb
@@ -0,0 +1,6 @@
+class AddProfileImageIdToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :profile_image_id, :string
+    add_column :users, :profile_body, :text
+  end
+end

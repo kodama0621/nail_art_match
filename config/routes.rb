d169a7e03338740b00658fcf7a8766c5cdde62cb
kodama0621 <eryuhikari@gmail.com>
2021-05-12 02:48:33 +0000

    [Add]プロフィール編集画面追加

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
     resource :likes, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :update]
 
 end

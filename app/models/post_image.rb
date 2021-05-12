cf4f7744c4dc6480a472a65e84916cf1d11c3fa8
kodama0621 <eryuhikari@gmail.com>
2021-05-12 11:58:54 +0000

    [Add]投稿機能にバリデーション追加

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -4,6 +4,10 @@ class PostImage < ApplicationRecord
   has_many :post_comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
+  validates :title, presence: true
+  validates :image, presence: true
+  validates :caption, presence: true
+
   def liked_by?(user)
     likes.where(user_id: user.id).exists?
   end

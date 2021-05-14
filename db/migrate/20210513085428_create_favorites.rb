afbbe49f04fe1941c0ea52b08109ebc67c750768
kodama0621 <eryuhikari@gmail.com>
2021-05-14 05:20:12 +0000

    [Update]ログイン,新規登録ページのレイアウト更新

--- db/migrate/20210513085428_create_favorites.rb
+++ db/migrate/20210513085428_create_favorites.rb
@@ -2,7 +2,7 @@ class CreateFavorites < ActiveRecord::Migration[5.2]
   def change
     create_table :favorites do |t|
 
-      t.references :user,   foreign_key: true
+      t.references :user, foreign_key: true
       t.references :post_image, foreign_key: true
 
       t.timestamps

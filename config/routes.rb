53241b91cff0ae2097e46b05d8e57c592e0dfec9
kodama0621 <eryuhikari@gmail.com>
2021-05-14 09:07:21 +0000

    [Update]aboutページのレイアウト更新

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  root 'homes#top'
-  get 'about/' => 'homes#about'
   get 'search' => 'post_images#search'
   devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'homes#top'
+  get 'about/' => 'homes#about'
+
   resources :post_images, only: [:new, :create, :index, :show, :edit, :update, :destroy] do
     resource :likes, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]

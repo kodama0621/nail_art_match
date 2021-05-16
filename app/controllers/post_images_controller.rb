aadcc77930377a37aba1a5a3637141f585481f03
kodama0621 <eryuhikari@gmail.com>
2021-05-16 17:38:36 +0000

    [Update]レイアウト更新(仮完成)

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -49,6 +49,7 @@ class PostImagesController < ApplicationController
   def destroy
     @post_image = PostImage.find(params[:id])
     @post_image.destroy
+    flash[:notice] = "投稿を削除しました。"
     redirect_to post_images_path
   end
 

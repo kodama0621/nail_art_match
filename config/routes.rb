59fa9e43e4a4523765eefd3b10b216ce220d3e52
kodama0621 <eryuhikari@gmail.com>
2021-05-18 02:58:35 +0000

    [Update]ゲストユーザ削除不可

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get '/search' => 'search#search'
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+  }
 
   root 'homes#top'
   get '/about' => 'homes#about'
